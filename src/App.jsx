f2361e2a03fea799624727f62418817b54186e42
pradeepasri26 <pradeepasri2003@gmail.com>
2024-07-05 14:55:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CharacterList from './CharacterList';
+
+const App = () => {
+  console.log('Rendering App');
+  return (
+    <div className="App">
+     
+      <CharacterList />
+    </div>
+  );
+};
+
+export default App;
